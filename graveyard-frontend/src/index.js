e09e8be43601d3dab0f453ccb16c4af8498fe6c7
mav-code <mavisansmembres@gmail.com>
2020-03-20 16:57:49 -0400

    basic layout, css, query selectors

--- graveyard-frontend/src/index.js
+++ graveyard-frontend/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+toggle = document.querySelector(".switch")
+ready = document.querySelector("aside").children[2]
+flowerCount = document.querySelector("aside").children[4]
+newGrave = document.querySelector("aside").children[6]
+// an event listener on toggle will:
+// 1. change page background color, whatever other css stuff, music even
+// 2. trigger the rise
+// 3. grey out + disable the toggle until all zombs in grave
