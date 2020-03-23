be54d32dc15d56a5b7f898a732d159c36550232f
Tororoi <cantwell.tom@gmail.com>
2020-03-23 09:31:26 -0400

    merged from mavi

--- graveyard-backend/db/schema.rb
+++ graveyard-backend/db/schema.rb
@@ -10,37 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_175434) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "corpses", force: :cascade do |t|
-    t.string "name"
-    t.integer "speed"
-    t.integer "flowers_needed"
-    t.bigint "grave_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["grave_id"], name: "index_corpses_on_grave_id"
-  end
-
-  create_table "flowers", force: :cascade do |t|
-    t.string "name"
-    t.bigint "grave_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["grave_id"], name: "index_flowers_on_grave_id"
-  end
-
-  create_table "graves", force: :cascade do |t|
-    t.string "name"
-    t.string "epitaph"
-    t.string "lifespan"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "corpses", "graves", column: "grave_id"
-  add_foreign_key "flowers", "graves", column: "grave_id"
 end
