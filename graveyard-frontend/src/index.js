f17fbfa8ac49e8328bb6689d896161b1bb295d6a
mav-code <mavisansmembres@gmail.com>
2020-03-23 01:36:45 -0400

    page loads with 3 random grave images

--- graveyard-frontend/src/index.js
+++ graveyard-frontend/src/index.js
@@ -1,3 +1,5 @@
+// Elements
+
 const toggle = document.querySelector(".switch")
 const ready = document.querySelector("aside").children[2]
 const flowerCount = document.querySelector("aside").children[4]
@@ -10,6 +12,33 @@ const plots = graveyard.children
 // 1. change page background color, whatever other css stuff, music even
 // 2. trigger the rise
 // 3. grey out + disable the toggle until all zombs in grave
+
+// Assets
+
+const openGraveDay = "open_grave.png"
+
+// Testing
+document.addEventListener('DOMContentLoaded', (event) => {
+    displayStartingGraves()
+})
+
+function displayStartingGraves(){
+    const randomPlots = shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]).slice(0, 3) // 3 random numbers btw 0 and 14
+    randomPlots.forEach(index => {
+        plots[index].innerHTML = `
+        <img src="open_grave.png" class="grave_img">
+        `
+    })
+}
+
+function shuffleArray(array) { // This is what it takes to generate several random unique numbers in a particular range in javascript, I guess 
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
 
 //Routes
 const gravesURL = `http://localhost:3000/graves/`
