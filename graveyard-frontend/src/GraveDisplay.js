243c3d9ffd396f936fa32f40733f4a2825e027b9
mav-code <mavisansmembres@gmail.com>
2020-03-24 15:59:20 -0400

    working on graves

---
+++ graveyard-frontend/src/GraveDisplay.js
@@ -0,0 +1,13 @@
+class GraveDisplay {
+    //coords assigned by plot instance
+    constructor(context, coords, graveObj) {
+        this.image = new Image()
+        this.image.src = "images/closed_grave_day.png"
+        this.coords.x = (coords.x * 192) + 80
+        this.coords.y = ((coords.y - 1) * 224) + 272
+        
+    }
+
+    draw() {
+        this.context.drawImage(this.image, this.coords.x, this.coords.y)
+      }
